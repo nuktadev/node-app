63413a2440a2a1b912e1e907b113743bbae18812
nuktadev <nukta.dev@gamil.com>
2024-06-30 11:28:48 +0600

    next level

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-app.get("/", (req, res) => res.send("Hello World From Docker!"));
+app.get("/", (req, res) =>
+  res.send("Hello World From Docker try to next level!")
+);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
